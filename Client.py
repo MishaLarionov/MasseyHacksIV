0e307d2ab576b4ba77117239893c42fd414dd6ff
Misha Larionov <MishaLarionov@users.noreply.github.com>
2018-04-29 10:49:51 -0400

    started handling keypresses (ignore the client testing)

--- Client.py
+++ Client.py
@@ -1,17 +1,22 @@
 import asyncio
+
+from random import random
+
 import websockets
 import time
 
 async def hello():
-    async with websockets.connect('ws://10.88.196.214:2018') as websocket:
+    async with websockets.connect('ws://localhost:2028') as websocket:
         name = input("What's your name? ")
         await websocket.send(name)
         print("> {}".format(name))
 
         greeting = await websocket.recv()
         print("< {}".format(greeting))
-        while True:
-            time.sleep(1)
-            await websocket.send('testing 123 from ' + name)
+        for i in range(0,1000):
+            if random() < 0.5:
+                await websocket.send('1C')
+            else:
+                await websocket.send('1E')
 
 asyncio.get_event_loop().run_until_complete(hello())
--- Server.py
+++ Server.py
@@ -4,19 +4,51 @@
 import asyncio
 import websockets
 
-connected = set()
+from keypresshandler import PressKey, ReleaseKey
 
+# Define the keys
+KEY_W = 0x57
+KEY_A = 0x41
+KEY_S = 0x53
+KEY_D = 0x44
+
+connected = set()
+lastDir = ["1E", "2E"]
+
+# Socket handler
 async def handler(websocket, path):
     global connected
-
+    global lastDir
     connected.add(websocket)
     try:
         await asyncio.wait([ws.send("register") for ws in connected])
     finally:
         while True:
-            print(await websocket.recv())
+            dir_input = await websocket.recv()
+            # Decide whether we got a new input
+            #   1C
+            # 1F  2F
+            #   2C
+            print(dir_input)
+            if dir_input.startswith("1") and dir_input != lastDir[0]:
+                # 1C = front
+                # 1F = left
+                ReleaseKey(KEY_W)
+                ReleaseKey(KEY_A)
+                if dir_input == "1C":
+                    PressKey(KEY_W)
+                elif dir_input == "1F":
+                    PressKey(KEY_A)
 
-start_server = websockets.serve(handler, None, 2018)
+                lastDir[0] = dir_input
+
+            elif dir_input.startswith("2") and dir_input != lastDir[1]:
+                # 2C = back
+                # 2F = right
+
+                lastDir[1] = dir_input
+
+start_server = websockets.serve(handler, None, 2028)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
